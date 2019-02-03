058a35e61267376cc048b132f4824f0addcc3728
Amy <amlydu@umich.edu>
2019-02-02 18:14:07 -0700

    Hook up controllers to make calls to the service class. Also add logic to create a Search object.

--- app/controllers/api/v1/where_to_eat_controller.rb
+++ app/controllers/api/v1/where_to_eat_controller.rb
@@ -1,10 +1,28 @@
 module Api
   module V1
     class WhereToEatController < ApplicationController
+      before_action :save_search_and_increment_count
+
       def options
+        response = yelp_service.search_for_restaurants(params: params)
+
+        json_response(response)
       end
 
       def just_tell_me
+        response = yelp_service.tell_me_where_to_eat(params: params)
+
+        json_response(response)
+      end
+
+      private
+
+      def save_search_and_increment_count
+        Search.where(query: request.url).first_or_create.increment!(:query_count)
+      end
+
+      def yelp_service
+        ExternalApis::Yelp.new
       end
     end
   end
