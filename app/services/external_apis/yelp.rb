4a18005af7f4229ec62528d3816950ac8e6f31a9
Amy <amlydu@umich.edu>
2019-02-02 17:18:25 -0700

    Initial Yelp defaults and setting reliable Constants

--- app/services/external_apis/yelp.rb
+++ app/services/external_apis/yelp.rb
@@ -3,5 +3,24 @@ require 'json'
 
 module ExternalApis
   class Yelp
+    ### Endpoint Information ###
+    #
+    API_HOST = 'https://api.yelp.com'
+    RESULT_LIMIT = 10
+
+    API_ENDPOINTS = {
+        businesses_search: '/v3/businesses/search',
+        businesses: '/v3/businesses/'
+    }
+
+    ### Default Yelp search params ###
+    #
+    DEFAULT_SEARCH_PARAMS = [
+        TERM = 'restaurants',
+        LOCATION = 'Denver, CO',
+        SEARCH_LIMIT = 35,
+        PRICE_STRING='1, 2, 3, 4',
+        MAX_RADIUS= 24
+    ]
   end
 end
