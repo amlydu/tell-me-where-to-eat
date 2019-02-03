183c88ddc3cbaa7da3538e30962df546f30f29d3
Amy <amlydu@umich.edu>
2019-02-02 19:50:40 -0700

    Add specs for when an unsuccessful call comes back from the YELP API that is any status other than OK.

--- spec/requests/where_to_eat_spec.rb
+++ spec/requests/where_to_eat_spec.rb
@@ -5,6 +5,7 @@ require "rails_helper"
 #
 RSpec.describe "Requesting where to eat", :type => :request do
   let(:response_json){ JSON.parse(response.body) }
+  let(:http_response_mock){ OpenStruct.new(parse: {}, status: OpenStruct.new(ok?: false, code: 400, reason: 'Bad Request')) }
 
   describe 'GET  /api/v1/where_to_eat/options' do
     context 'successful call with Default search params' do
@@ -27,6 +28,25 @@ RSpec.describe "Requesting where to eat", :type => :request do
 
     it 'creates a new Search and increments counter' do
       expect{  get '/api/v1/where_to_eat/options' }.to change{ Search.count }.from(0).to(1)
+    end
+
+    context 'Unsuccessful call due to errors returned from Yelp API' do
+      before do
+        allow_any_instance_of(ExternalApis::Yelp).to receive(:get_request_to_yelp_api).and_return(http_response_mock)
+
+        get '/api/v1/where_to_eat/options'
+      end
+
+      it 'returns a 422 Unprocessable Entity to the caller' do
+        expect(response.status).to eq(422)
+      end
+
+      it 'returns json indicating an error occurred' do
+        expect(response_json).not_to be_empty
+        expect(response_json['external_api']['status_code']).to eq(400)
+        expect(response_json['external_api']['status_reason']).to eq('Bad Request')
+        expect(response_json['error']).to eq('CustomException::Apis::UnableToGetResults')
+      end
     end
   end
 
@@ -51,7 +71,26 @@ RSpec.describe "Requesting where to eat", :type => :request do
     end
 
     it 'creates a new Search and increments counter' do
-      expect{  get '/api/v1/where_to_eat/options' }.to change{ Search.count }.from(0).to(1)
+      expect{  get '/api/v1/where_to_eat/just_tell_me' }.to change{ Search.count }.from(0).to(1)
+    end
+
+    context 'Unsuccessful call due to errors returned from Yelp API' do
+      before do
+        allow_any_instance_of(ExternalApis::Yelp).to receive(:get_request_to_yelp_api).and_return(http_response_mock)
+
+        get '/api/v1/where_to_eat/just_tell_me'
+      end
+
+      it 'returns a 422 Unprocessable Entity to the caller' do
+        expect(response.status).to eq(422)
+      end
+
+      it 'returns json indicating an error occurred' do
+        expect(response_json).not_to be_empty
+        expect(response_json['external_api']['status_code']).to eq(400)
+        expect(response_json['external_api']['status_reason']).to eq('Bad Request')
+        expect(response_json['error']).to eq('CustomException::Apis::UnableToGetResults')
+      end
     end
   end
 end
