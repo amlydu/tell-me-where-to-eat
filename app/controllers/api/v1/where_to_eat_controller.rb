aed1a957ff972f2aac6f107d98ee255cbc3c228d
Amy <amlydu@umich.edu>
2019-02-02 17:08:46 -0700

    Add initial controller methods and routes.

---
+++ app/controllers/api/v1/where_to_eat_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class WhereToEatController < ApplicationController
+      def options
+      end
+
+      def just_tell_me
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'where_to_eat/options'
+      get 'where_to_eat/just_tell_me'
+    end
+  end
 end
